d53bb017f6ca509957345140f3bacd0d9dd1725c
Mishel Tyurkova <misheltyurkova@gmail.com>
2023-06-28 19:13:39 +1000

    Finished Lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import About from "../components/About.js";
+import NavBar from "../components/NavBar.js";
+import Home from "../components/Home.js";
 
 function App() {
   return (
@@ -11,3 +14,5 @@ function App() {
 }
 
 export default App;
+
+
